a05eff1592f63bbad8d73d238242b78f43ab2af1
Lengochuy13 <lengochuyc2130220002@gmail.com>
2022-04-19 14:49:53 +0700

    first commit

---
+++ assets/js/fame.js
@@ -0,0 +1,19 @@
+VanillaTilt.init(document.querySelector(".glassmorphic-card"), {
+  max: 15,
+  speed: 200,
+  glare: true,
+  "max-glare": 1,
+});
+
+VanillaTilt.init(document.querySelector(".glassmorphic-card2"), {
+  max: 15,
+  speed: 200,
+  glare: true,
+  "max-glare": 1,
+});
+VanillaTilt.init(document.querySelector(".glassmorphic-card3"), {
+  max: 15,
+  speed: 200,
+  glare: true,
+  "max-glare": 1,
+});
